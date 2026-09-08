ceb5849b7f95b63aebef8454aed4eb5af2c708c5
sasha2505mail-ai <sasha2505mail@gmail.com>
2026-09-08 23:18:24 +0700

    Add QnnCommon.h header file for QNN logging

---
+++ work/android/app/src/main/cpp/include/QNN/QnnCommon.h
@@ -0,0 +1,12 @@
+#ifndef QNN_COMMON_H
+#define QNN_COMMON_H
+
+#define QNN_API 
+
+typedef enum {
+    QNN_LOG_LEVEL_ERROR = 0,
+    QNN_LOG_LEVEL_WARN = 1,
+    QNN_LOG_LEVEL_INFO = 2
+} Qnn_LogLevel_t;
+
+#endif // QNN_COMMON_H
